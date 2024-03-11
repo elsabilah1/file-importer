67c2cc410f5e09e2cda7c377bb52c2c128270169
elsabilah1 <elsabilah1@gmail.com>
2024-03-11 13:51:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,89 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as XLSX from "xlsx";
+import { z } from "zod";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: []
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'file-importer-app';
+  excelFile: File | undefined;
+  status: string | undefined;
+  previewJSON: string | undefined;
+  isProcessing: boolean = false;
+  errors: any[] = [];
+
+  getBadgeClass() {
+    return "badge " + this.status?.toLowerCase();
+  }
+
+  onSelect(event: any) {
+    this.excelFile = event.addedFiles[0];
+    this.status = "Draft";
+  }
+
+  onRemove() {
+    this.excelFile = undefined;
+    this.status = undefined;
+    this.previewJSON = undefined;
+    this.errors = [];
+  }
+
+  handleImport() {
+    if (!this.excelFile) return;
+
+    this.previewJSON = undefined;
+    this.isProcessing = true;
+
+    // read excel file
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(this.excelFile!);
+    let binaryData;
+
+    fileReader.onload = (event: any) => {
+      binaryData = event.target.result;
+      let workbook = XLSX.read(binaryData, { type: "binary" });
+
+      // parse to json format
+      const data = XLSX.utils.sheet_to_json(workbook.Sheets["Sheet1"]);
+
+      // validate data
+      const dataSchema = z.object({
+        ID: z.coerce.string().length(8),
+        NAME: z.string(),
+        GENDER: z.number().min(1).max(2),
+        "BIRTH DATE": z.coerce.date(),
+        "BIRTH PLACE": z.string().optional(),
+      });
+
+      data.map((item, i) => {
+        const result = dataSchema.safeParse(item);
+        if (!result.success) {
+          const formatted = result.error.issues;
+          this.errors.push({
+            row: i + 1,
+            errors: formatted,
+          });
+        }
+      });
+
+      // console.log(this.errors);
+
+      this.isProcessing = false;
+      if (this.errors.length > 0) {
+        this.status = "failed";
+        return;
+      }
+
+      // if no errors process the data
+      /*
+       * such as saving data to db
+       * write the script here
+       */
+
+      // example preview data
+      this.previewJSON = JSON.stringify(data, undefined, 4);
+      this.status = "success";
+    };
+  }
 }
